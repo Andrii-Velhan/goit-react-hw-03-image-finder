2f514022f6d0fb109e6b22529b440202bb0f04c1
Andrii-Velhan <velganaa@gmail.com>
2021-01-29 23:03:26 +0200

    add Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Modal from './components/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <>
+        <button className="Button" type="button" onClick={this.toggleModal}>
+          Open modal
+        </button>
+
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <button className="Button" type="button" onClick={this.toggleModal}>
+              Close modal
+            </button>
+            <h1>modal content</h1>
+            <img src="" alt="" />
+          </Modal>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import './Modal.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    // console.log(e.code);
+
+    if (e.code === 'Escape') {
+      // console.log('pressed Escape, modal must be closed');
+
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      // console.log('clicked to backdrop');
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className="Overlay" onClick={this.handleBackdropClick}>
+        <div className="Modal">{this.props.children}</div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
